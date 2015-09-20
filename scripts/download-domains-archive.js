53e56e019b1a3d1a89cea74d9c698ead645f5067
Michal Stankoviansky <michal@stankoviansky.com>
2015-09-20 14:49:21 +0200

    Minor changes - extracting dl script config options to the top

--- scripts/download-domains-archive.js
+++ scripts/download-domains-archive.js
@@ -2,16 +2,21 @@ var fs = require('fs');
 var url = require('url');
 var https = require('https');
 
+var options = {
+	requestHeaders: {
+		"Referer": "https://www.sk-nic.sk/main.jsp",
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36"
+	}
+};
+
 module.exports = function (from, to) {
 	var file = fs.createWriteStream(to);
 	var parsedUrl = url.parse(from);
+	
 	var reqOptions = {
 		hostname: parsedUrl.hostname,
 		path: parsedUrl.path,
-		headers: {
-			"Referer": "https://www.sk-nic.sk/main.jsp", 
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36"
-		}
+		headers: options.requestHeaders
 	};
 
 	return new Promise(function (resolve, reject) {
