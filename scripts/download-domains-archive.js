48d80aa1686b00026e081af6b8de115540e46b50
Michal Stankoviansky <michal@stankoviansky.com>
2015-09-23 12:44:33 +0200

    Added extend dependency + new param "options" to downloadDomainsArchive()

--- scripts/download-domains-archive.js
+++ scripts/download-domains-archive.js
@@ -1,15 +1,21 @@
+/*
+Download file from url specified in "from",
+save it to path specified in "to"
+*/
 var fs = require('fs');
 var url = require('url');
 var https = require('https');
+var extend = require('extend');
 
-var options = {
+var defaultOptions = {
 	requestHeaders: {
 		"Referer": "https://www.sk-nic.sk/main.jsp",
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36"
 	}
 };
 
-module.exports = function (from, to) {
+module.exports = function (from, to, options) {
+	var options = extend(defaultOptions, options);
 	var file = fs.createWriteStream(to);
 	var parsedUrl = url.parse(from);
 	
