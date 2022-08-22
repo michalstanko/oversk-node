1995f352dc5762523cbfde5b051e5a25cf8accab
Michal Stankoviansky <michal@stankoviansky.com>
2022-08-22 22:12:06 +0200

    sql - set default charset to utf8mb4

--- sql/db.sql
+++ sql/db.sql
@@ -3,4 +3,4 @@ CREATE TABLE `domains` (
   `domain_name` VARCHAR(255) NOT NULL,  
   PRIMARY KEY (`id`),
   UNIQUE KEY `domain_name_unique` (`domain_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
